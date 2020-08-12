e2a21eee7ccf820af4864056365b1c7d8ecd45e0
Jordan <jordynwinnie@gmail.com>
2020-08-12 14:04:57 +0800

    Added PlayerlockLogic
    Ensures player does not fly out of the map

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    public enum PlayerPositionState { Right, Left, Middle };
 
-
-
+    public PlayerPositionState playerPosition;
     [SerializeField] float jumpSpeed = 10.0f;
     [SerializeField] float gravity = 20.0f;
     public bool isGrounded = false;
@@ -17,21 +17,46 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        playerPosition = PlayerPositionState.Middle;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (playerPosition)
+        {
+            case PlayerPositionState.Right:
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    playerPosition = PlayerPositionState.Middle;
+                    transform.Translate(Vector3.left * 5f);
+                }
+                break;
+            case PlayerPositionState.Left:
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    playerPosition = PlayerPositionState.Middle;
+                    transform.Translate(Vector3.right * 5f);
+                }
+                break;
+            case PlayerPositionState.Middle:
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    //Going Left:
+                    playerPosition = PlayerPositionState.Left;
+                    transform.Translate(Vector3.left * 5f);
+                }
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    //GOing right:
+                    playerPosition = PlayerPositionState.Right;
+                    transform.Translate(Vector3.right * 5f);
+                }
+                break;
+            default:
+                break;
+        }
         
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * 5f);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * 5f);
-        }
 
         //print(rb.velocity);
         if (Input.GetButtonDown("Jump") && isGrounded)
